e1035eecb2311fa038943cbeef36433d7a338684
Lidia Karpinski <lidiakarpinski@gmail.com>
2022-07-20 11:43:19 -0400

    Added resCards and css for res container

--- src/App/ReservationCard.js
+++ src/App/ReservationCard.js
@@ -2,7 +2,13 @@ import React from 'react';
 import './ReservationCard.css';
 
 
-
+const ReservationCard = () => {
+    return (
+        <div>
+            <h3>Res Card</h3>
+        </div>
+    )
+}
 
 
 
